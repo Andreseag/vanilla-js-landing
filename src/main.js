5d2732e8e025a0ac3f3edc8924624cc64c69b0de
Edwin Castro <edwin.castro@agenciamatch.cl>
2025-01-26 17:08:51 -0500

    feat: init project :sparkles:

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import "./styles/style.scss";
+import { setupCounter } from "./counter.js";
+
+// document.querySelector("#app").innerHTML = `
+//   <header class="header">
+//     <div class="header__container">
+//       <div class="header__logo">
+//         <p>LOGOTIPO</p>
+//       </div>
+//       <div class="header__info">
+//         <h1>
+//           No pierdas ventas este Cyber
+//         </h1>
+//         <p>
+//           Si temes que tu Web se caiga o no cargue bien durante el Cyber, nosotros te podemos ayudar.
+//         </p>
+//       </div>
+//     </div>
+//   </header>
+// `;
+
+setupCounter(document.querySelector("#counter"));
